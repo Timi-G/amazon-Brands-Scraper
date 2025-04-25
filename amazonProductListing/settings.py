6f77fbb3b11f893bc19d57f9cbeb87c2c785e212
Timi-G <rotimiolasehinde.a@gmail.com>
2025-04-25 17:02:34 +0100

    Added CELERY_BEAT_SCHEDULER so scheduler command can be used to run celery beat in terminal

--- amazonProductListing/settings.py
+++ amazonProductListing/settings.py
@@ -119,9 +119,10 @@ USE_I18N = True
 USE_TZ = True
 
 # celery settings
-result_backend = 'redis://localhost/0'
+CELERY_RESULT_BACKEND = 'redis://localhost/0'
 CELERY_BROKER_URL = 'redis://localhost:6379/0'  # URL of the Redis server
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 
 # Static files (CSS, JavaScript, Images)
